182607abf6a85ab859116dc3cf3d645b5860adab
Ankur Prakash <ankur.prakash@mavenir.com>
2022-01-27 19:56:38 +0530

    Added SL and SDK

---
+++ RequestSDK/RequestSDK/RequestSDK.h
@@ -0,0 +1,18 @@
+//
+//  RequestSDK.h
+//  RequestSDK
+//
+//  Created by Ankur Prakash on 27/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RequestSDK.
+FOUNDATION_EXPORT double RequestSDKVersionNumber;
+
+//! Project version string for RequestSDK.
+FOUNDATION_EXPORT const unsigned char RequestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RequestSDK/PublicHeader.h>
+
+
---
+++ RequestSL/RequestSL/RequestSL.h
@@ -0,0 +1,18 @@
+//
+//  RequestSL.h
+//  RequestSL
+//
+//  Created by Ankur Prakash on 27/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RequestSL.
+FOUNDATION_EXPORT double RequestSLVersionNumber;
+
+//! Project version string for RequestSL.
+FOUNDATION_EXPORT const unsigned char RequestSLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RequestSL/PublicHeader.h>
+
+
